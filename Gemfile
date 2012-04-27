ad224a4277cc64058239fe00181e83e61ced8ae3
Allan Ripley <allan@arbiontec.com>
2012-04-26 20:54:55 -0700

    Finish layout changes

--- Gemfile
+++ Gemfile
@@ -1,18 +1,19 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'bootstrap-sass', '2.0.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3', '1.3.5'
-gem 'rspec-rails', '2.9.0'
-
+gem 'guard-rspec', '0.5.5'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.4'
+  gem 'rspec-rails', '2.9.0'
   gem 'coffee-rails', '~> 3.2.2'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
@@ -37,6 +38,11 @@ gem 'jquery-rails', '2.0.0'
 
 group :test do
 	gem 'capybara', '1.1.2'
+	gem 'rspec-rails', '2.9.0'
+	gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,44 +1,28 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+subject { page }
   describe "Home page" do
-
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => "Sample App")
-    end
-
-    it "should have the title 'Home'" do
-	  visit '/static_pages/home'
-  	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-    end
+    before { visit root_path }
+    it { should have_selector('h1', :text => "Sample App")}
+    it { should have_selector('title', :text => full_title('')) }
   end
 
   describe "Help Page" do
-  	it "should have the h1 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('h1', :text => 'Help')
-  	end
-
-    it "should have title 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+    before { visit help_path }
+  	it { should have_selector('h1', :text => 'Help')}
+    it { should have_selector('title', :text => full_title('Help'))}
   end
   
   describe "About Page" do
-  	it "ahould have the h1 'About'" do
-  		visit "/static_pages/about"
-  		page.should have_selector('h1', :text => 'About')
-  	end
-
-  	it "should have title 'About'" do
-  	visit '/static_pages/about'
-  	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
-    end
+    before { visit '/about'}
+  	it { should have_selector('h1', :text => 'About')}
+  	it { should have_selector('title', :text => full_title('About'))}
   end
-  
-  		
 
+  describe "Contact page" do
+    before { visit '/contact'}
+    it { should have_selector('h1', :text => 'Contact')}
+    it { should have_selector('title', :text => full_title('Contact'))}
+  end
 end
